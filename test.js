3dc04eb9fe4dfa7eeca0b78ede49a49dc6cca464
Bcape2 <101348735+Bcape2@users.noreply.github.com>
2022-03-29 19:09:52 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,26 +1,92 @@
-var fs = require('fs'); //hello@
+/*
+Needed attributes from index:
+Full Name  JSON.parse(index).faculty.fullName;
+Email      JSON.parse(index).faculty.email;
+Phone #    JSON.parse(index).faculty.phone;
+Building   JSON.parse(index).building;
+RoomNum    JSON.parse(index).faculty.roomNum;
+
+Needed attributes from Classes2022:
+CRN        JSON.parse(classes).Classes.SSBSECT_CRN;
+Subj_Code  JSON.parse(classes).Classes.SSBSECT_CODE;
+Course #   JSON.parse(classes).Classes.SSBSECT_NUMB;
+Title      JSON.parse(classes).Classes.COURSE_TITLE;
+Days       JSON.parse(classes).Classes.DAYS;
+Class Time JSON.parse(classes).Classes.CLASS_TIME;
+Location   JSON.parse(classes).Classes.LOCATION;
+Instructor JSON.parse(classes).Classes.INSTRUCTOR;
+(try to link with index to get info)
+Class type JSON.parse(classes).Classes.METHOD;
+Credit Hrs JSON.parse(classes).Classes.CREDIT_HOURS;
+*/
+/*
+var classfile;
+function button(){
+    fetch('Classes2022.json')
+    .then(response => response.json())
+    .then(data => {
+       data.forEach(Classes => {
+           
+       })
+           
+       }); 
+};
+*/
+
+//Faculty Variables
+var emp_name, email, phone, building, office;
+//Class variables
+var CRN,subj_Code,courseNum,title, days, Time, CLocation, Instructor, CType, Hrs;
+
+function button(){
+    var searchtxt = document.getElementById("search-input").value;
+
+    fetch('Classes2022.json')
+    .then(response => response.json())
+    .then(data => {
+    
+    CRN = JSON.stringify(data.Classes.SSBSECT_CRN);
+    document.getElementById("demo").innerText = CRN;
+
+    });
+
+}
+
+function Create(){
+
+}
+
+/*foreach loop to search through 
+
+/* using fs to parse json.
+(not sure how to integrate searchbar function)
+
+var fs = require('fs');
 // make sure to have correct json file here otherwise error will occur
-fs.readFile('index.json', 'utf8', function (err, data) {
+fs.readFile('index.json', 'utf8', function (err, index) {
     if (err){
         console.log(err);
     }
     else {
         // Select department from faculty at index 0
-        console.log(JSON.parse(data).faculty[0].departments);
-        
-       /* Select CRN from Classes array at index 0
-        console.log(JSON.parse(data).Classes[0].SSBSECT_CRN);]
-        */
-    }
-}) 
+        facultyInfo = JSON.parse(index);
+        console.log(JSON.parse(index).faculty[0]);
 
-/*
-search crn 
-    give instructor perry, shannon
-    in index.json shannon perry
-    perry, shannon = shannon perry
+    }
+});
+fs.readFile('Classes2022.json','utf8', function(err,classes){
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log(JSON.parse(classes).Classes[0]);
+        }
+});
 */
-/*
+
+/* Using Fetch API.
+Works with search bar, just have to figure out how to make it work now without database
+
 const searchBtn = document.querySelector('#search-btn');
 
 searchBtn.onclick = function (){
@@ -60,4 +126,33 @@ app.get('/search/:TYPE' , (request, response) => {
     }
 }
 }
+*/
+
+/* using Fetch API again
+Need to work on how to make searchbar work
+const searchClasses = async searchText => {
+    const res = await fetch {'/Classes2022.json'};
+    const classes = await res.json();
+
+    //Get matches to current text input
+    let matches = classes.filter(class => {
+        const regex = new RegExp(`${searchText}`, 'gi');
+        return class.COURSE_TITLE.match(regex)
+    });
+
+
+};
+
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches.map(match => `
+        <div class="card card-body mb-1">
+            <h4>${match.COURSE_TITLE}<h4>
+        </div>
+        `).join('');
+
+        matchList.innerHTML = html;
+    }
+}
+search.addEventListener('input', () => searchClasses(search.value));
 */
