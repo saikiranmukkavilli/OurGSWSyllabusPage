c191315025e70f7f1667dd96fe09d3791fafe142
Bcape2 <101348735+Bcape2@users.noreply.github.com>
2022-03-29 20:19:57 -0400

    Update test.js

--- test.js
+++ test.js
@@ -19,140 +19,71 @@ Instructor JSON.parse(classes).Classes.INSTRUCTOR;
 Class type JSON.parse(classes).Classes.METHOD;
 Credit Hrs JSON.parse(classes).Classes.CREDIT_HOURS;
 */
-/*
-var classfile;
-function button(){
-    fetch('Classes2022.json')
-    .then(response => response.json())
-    .then(data => {
-       data.forEach(Classes => {
-           
-       })
-           
-       }); 
-};
-*/
 
 //Faculty Variables
 var emp_name, email, phone, building, office;
+
 //Class variables
-var CRN,subj_Code,courseNum,title, days, Time, CLocation, Instructor, CType, Hrs;
+var CRN, subj_Code, courseNum, title, days, time, cLocation, instructor, cType, hrs;
 
 function button(){
     var searchtxt = document.getElementById("search-input").value;
+
+    fetch('index.json')
+    .then(response => response.json())
+    .then(data => {
+        
+        emp_name = JSON.stringify(data.faculty.fullName);
+        email = JSON.stringify(data.faculty.email);
+        phone =JSON.stringify(data.faculty.phone);
+        building = JSON.stringify(data.faculty.building);
+        office = JSON.stringify(data.faculty.roomNum);
+    });
 
     fetch('Classes2022.json')
     .then(response => response.json())
     .then(data => {
     
     CRN = JSON.stringify(data.Classes.SSBSECT_CRN);
-    document.getElementById("demo").innerText = CRN;
-
+    subj_Code = JSON.stringify(data.Classes.SSBSECT_CRN);
+    courseNum = JSON.stringify(data.Classes.SSBSECT_CRN);
+    title = JSON.stringify(data.Classes.SSBSECT_CRN);
+    days = JSON.stringify(data.Classes.SSBSECT_CRN);
+    time = JSON.stringify(data.Classes.SSBSECT_CRN);
+    cLocation= JSON.stringify(data.Classes.SSBSECT_CRN);
+    instructor= JSON.stringify(data.Classes.SSBSECT_CRN);
+    cType= JSON.stringify(data.Classes.SSBSECT_CRN);
+    hrs= JSON.stringify(data.Classes.SSBSECT_CRN);
+    Create();
     });
 
 }
 
 function Create(){
+    var div = document.getElementById('demo');
+    var para = document.createElement('p');
+    var node = document.createTextNode("");
 
+
+    para.appendChild(node);
+    div.appendChild(para);
 }
 
-/*foreach loop to search through 
+var clickCounter = 0;
 
-/* using fs to parse json.
-(not sure how to integrate searchbar function)
+function Reset(){
+    
+    clickCounter += 1;
 
-var fs = require('fs');
-// make sure to have correct json file here otherwise error will occur
-fs.readFile('index.json', 'utf8', function (err, index) {
-    if (err){
-        console.log(err);
-    }
-    else {
-        // Select department from faculty at index 0
-        facultyInfo = JSON.parse(index);
-        console.log(JSON.parse(index).faculty[0]);
+    if (clickCounter == 1)
+        alert("WARNING!!!This will completely reset your form.\n"+
+        "You will have to enter all data again.\n"+
+        "If you are sure you want to delete your work click delete again.");
 
-    }
-});
-fs.readFile('Classes2022.json','utf8', function(err,classes){
-        if (err){
-            console.log(err);
-        }
-        else{
-            console.log(JSON.parse(classes).Classes[0]);
-        }
-});
-*/
-
-/* Using Fetch API.
-Works with search bar, just have to figure out how to make it work now without database
-
-const searchBtn = document.querySelector('#search-btn');
-
-searchBtn.onclick = function (){
-    const searchValue = document.querySelector('#search-input').value;
-
-    fetch('http://localhost:5000/search/'+ searchValue)
-    .then(response => response.json())
-    .then(data => loadHTMLTable(data['data']));
-}
-
-//search
-app.get('/search/:TYPE' , (request, response) => {
-    const { FIRST_NAME } = request.params;
-    const db = dbService.getDbServiceInstance();
-   
-    const result = db.searchByType(FIRST_NAME);
-       
-       result
-       .then(data => response.json({data : data}))
-       .catch(err => console.log(err));
-   })
-
-   async searchByType(TYPE){
-    try {
-        const response  = await new Promise((resolve, reject) => {
-            const query = "SELECT * FROM weapons WHERE TYPE = ?;";
-
-            connection.query(query, [TYPE], (err, results) => {
-                if (err) reject(new Error(err.message));
-                resolve(results);
-            })
-        });
-        return response;
-    }catch(error){
-
-    console.log(error);
+    else if (clickCounter == 2){
+        location.reload()
+        clickCounter = 0;
     }
 }
-}
-*/
 
-/* using Fetch API again
-Need to work on how to make searchbar work
-const searchClasses = async searchText => {
-    const res = await fetch {'/Classes2022.json'};
-    const classes = await res.json();
-
-    //Get matches to current text input
-    let matches = classes.filter(class => {
-        const regex = new RegExp(`${searchText}`, 'gi');
-        return class.COURSE_TITLE.match(regex)
-    });
-
-
-};
-
-const outputHtml = matches => {
-    if (matches.length > 0) {
-        const html = matches.map(match => `
-        <div class="card card-body mb-1">
-            <h4>${match.COURSE_TITLE}<h4>
-        </div>
-        `).join('');
-
-        matchList.innerHTML = html;
-    }
-}
-search.addEventListener('input', () => searchClasses(search.value));
-*/
+//foreach loop to search through 
